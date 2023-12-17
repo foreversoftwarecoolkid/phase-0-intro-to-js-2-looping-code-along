3fa863d0308c1a863e74d673e36015b99a0c222c
Henry <henry.bogita@student.moringaschool.com>
2023-12-17 15:40:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+const writeCards = (names, event) => {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+   };
+   
+   const countDown = (num) => {
+    for (let i = num; i >= 0; i--) {
+       console.log(i);
+    }
+   };
+   
+   module.exports = { writeCards, countDown };
